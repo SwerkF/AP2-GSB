7d9c906d31c458c5589cf1f8ffdbaf3b1772569c
Thomas <thomas.letellier28500@gmail.com>
2022-11-18 16:32:37 +0100

    <main> Initial commit

---
+++ Projet AP2/Projet AP2/Form1.cs
@@ -0,0 +1,10 @@
+namespace Projet_AP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Projet AP2/Projet AP2/Globale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_AP2
+{
+    internal class Globale
+    {
+        private Dictionary<string, Famille> lesFamilles;   //Liste des familles
+        private Dictionary<string, Medicament> LesMedicaments;      //Liste des médicaments
+    }
+}
