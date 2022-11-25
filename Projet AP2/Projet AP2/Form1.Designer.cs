e3c27f007b982b193bab6ba4b8ea6cec385a2fad
Thomas <thomas.letellier28500@gmail.com>
2022-11-25 14:03:20 +0100

    <dev> initial commit

--- Projet AP2/Projet AP2/Form1.Designer.cs
+++ Projet AP2/Projet AP2/Form1.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_connecter = new System.Windows.Forms.Button();
+            this.tb_user = new System.Windows.Forms.TextBox();
+            this.tb_mdp = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::Projet_AP2.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(371, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.pictureBox2.Location = new System.Drawing.Point(167, 69);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(564, 402);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(209, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nom d\'Utilisateur :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(209, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mot de passe :";
+            // 
+            // btn_connecter
+            // 
+            this.btn_connecter.BackColor = System.Drawing.Color.LightBlue;
+            this.btn_connecter.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_connecter.Location = new System.Drawing.Point(371, 389);
+            this.btn_connecter.Name = "btn_connecter";
+            this.btn_connecter.Size = new System.Drawing.Size(149, 45);
+            this.btn_connecter.TabIndex = 4;
+            this.btn_connecter.Text = "SE CONNECTER";
+            this.btn_connecter.UseVisualStyleBackColor = false;
+            this.btn_connecter.Click += new System.EventHandler(this.btn_connecter_Click);
+            // 
+            // tb_user
+            // 
+            this.tb_user.Location = new System.Drawing.Point(355, 228);
+            this.tb_user.Name = "tb_user";
+            this.tb_user.Size = new System.Drawing.Size(194, 23);
+            this.tb_user.TabIndex = 5;
+            // 
+            // tb_mdp
+            // 
+            this.tb_mdp.Location = new System.Drawing.Point(355, 302);
+            this.tb_mdp.Name = "tb_mdp";
+            this.tb_mdp.Size = new System.Drawing.Size(194, 23);
+            this.tb_mdp.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(934, 538);
+            this.Controls.Add(this.tb_mdp);
+            this.Controls.Add(this.tb_user);
+            this.Controls.Add(this.btn_connecter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Label label2;
+        private Button btn_connecter;
+        private TextBox tb_user;
+        private TextBox tb_mdp;
     }
 }
--- Projet AP2/Projet AP2/Form1.cs
+++ Projet AP2/Projet AP2/Form1.cs
@@ -6,5 +6,38 @@ namespace Projet_AP2
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_connecter_Click(object sender, EventArgs e)
+        {
+            if(tb_user.Text == "Administrateur" && tb_mdp.Text == "Admin123")
+            {
+                new Form2().Show();
+                new Form1().Close();
+            }
+            else
+            {
+                MessageBox.Show("Une des données est fausse !");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set to no text.
+            tb_mdp.Text = "";
+            // The password character is an asterisk.
+            tb_mdp.PasswordChar = '*';
+            // The control will allow no more than 14 characters.
+            tb_mdp.MaxLength = 14;
+        }
     }
 }
